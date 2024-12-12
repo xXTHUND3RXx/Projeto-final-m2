05d2756909ffc1e2e840a08f24e18481e9cd50e4
NathaliaMartins <nathaliafloresmartins4550@gmail.com>
2024-12-12 00:54:57 -0300

    merge branch dev-Nathalia

---
+++ src/js/ajudar.js
@@ -0,0 +1,54 @@
+import urgencyNumbers from"../js/telefones.js"
+
+const displayInfo = document.getElementById("display")
+
+
+//procurar na lista telefonica com base no parametro
+function numberByState(state){
+  const search = urgencyNumbers.find(estado => estado.estado ==state)
+  if(search){
+    return search
+  }
+}
+
+//altera o texto para o encontrado na lista
+function displayer(obj){
+
+  
+  displayInfo.innerHTML = 
+  `
+  
+    <p>${obj.estado}</p>
+    <p>${obj.bombeiros}</p>
+    <p>${obj.defesaCivil}</p>
+    <p>${obj.meioAmbiente}</p>
+  
+    
+  `
+}
+
+//ao carregar a pagina
+window.onload = function(){
+  //id da caixa de selecao
+  const selectElement = document.getElementById('state-select');
+  //preenche o select automaticamente com todos os estados da lista
+  const nomesEstados = urgencyNumbers.map(estado => estado.estado);
+ 
+  //pra cada estado 
+  nomesEstados.forEach(nomeEstado =>{
+    //cria, preenche
+    const option = document.createElement('option');
+    option.value = nomeEstado;
+    option.textContent = nomeEstado
+    //e adiciona
+    selectElement.appendChild(option);
+  })
+
+  //qual foi selecionado
+  selectElement.addEventListener('change',event =>{
+    const selectedValue = event.target.value;
+    displayer(numberByState(selectedValue))
+    
+  })
+}
+
