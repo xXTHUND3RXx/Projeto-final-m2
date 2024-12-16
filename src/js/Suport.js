7f6e73cc6047d82856a4716593f9bc73a2c96793
DgDevHub <cavaleirooficia@gmail.com>
2024-12-15 21:11:13 -0300

    :construction: build: Criando components da inicio.html e implementação de sistema na sobre.html

--- src/js/Suport.js
+++ src/js/Suport.js
@@ -3,14 +3,25 @@ let currentAudio = null;
 
 const toggleButton = document.getElementById("toggle-audio");
 const elementsWithAudio = document.querySelectorAll("[data-audio]");
+const articleSobre = document.getElementById("frame")
+const sectionSobre = document.getElementById("container-painel")
 
 toggleButton.addEventListener("click", () => {
     audioEnabled = !audioEnabled;
+
     toggleButton.textContent = audioEnabled ? "Desativar Áudio" : "Ativar Áudio";
 
     if (!audioEnabled && currentAudio) {
         currentAudio.pause();
         currentAudio = null;
+    }
+
+    if (audioEnabled) {
+        articleSobre.style.display = "none";
+        sectionSobre.style.height = "30rem";
+    } else {
+        articleSobre.style.display = "flex";
+        sectionSobre.style.height = "auto";
     }
 });
 
